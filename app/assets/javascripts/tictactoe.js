8e10ddeacacca30dcc24999a93f9bc3d24f9768d
Brad Carlson <b2carlso@yahoo.com>
2018-09-13 21:20:20 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -10,3 +10,26 @@ var updateState = function(square) {
 var setMessage = function(string) {
   $('#message').text(string);
 }
+
+var checkWinner = function() {
+   const WIN_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  var board = {};
+  var winner = false;
+  $('td').text((index, square) => board[index] = square);
+   WIN_COMBOS.forEach(function(combo) {
+    if (board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]] && board[combo[0]] != "") {
+      winner = true;
+      setMessage(`Player ${board[combo[0]]} Won!`);
+    }
+  });
+  return winner;
+};
